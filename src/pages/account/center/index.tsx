4fb35fb6d1ab5e2ad9ab5edf259953ceac09f278
Null <gamelifezhao@163.com>
2023-05-26 19:11:25 +0800

    feat: q s
    1

--- src/pages/account/center/index.tsx
+++ src/pages/account/center/index.tsx
@@ -3,7 +3,7 @@ import { history } from 'umi';
 export default () => {
   return (
     <div className={styles.background}>
-      <h1>下面为代码示例，详细请看center目录下的md文件</h1>
+      <h1>下面为代码示例，详细请看center目录下的md文件1</h1>
       <a onClick={() => history.push('/hooks')}>基础hooks复习</a>
       <br />
       <a onClick={() => history.push('/4-26')}>自定义hooks,hoc组件编写,以及react父调子函数复习</a>
